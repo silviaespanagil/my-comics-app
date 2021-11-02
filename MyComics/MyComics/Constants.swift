818b3545cd6795a2f9fbd3397300a76578bdbd13
Silvia España <silviaespanagil@gmail.com>
2021-11-02 17:25:18 +0100

    Add Constants file

---
+++ MyComics/MyComics/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  MyComics
+//
+//  Created by Silvia España on 2/11/21.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let baseURL = "https://comicvine.gamespot.com/api/"
+    static let apiKey = "Your apiKey here"
+}
